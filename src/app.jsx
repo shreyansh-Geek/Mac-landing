9e4688e52b328827a9341bd4bfb307c466c5a884
Shreyansh <shreyanshloop07@gmail.com>
2025-11-25 19:23:16 +0530

    implement scroll-triggered showcase section with GSAP animations and masking

--- src/app.jsx
+++ src/app.jsx
@@ -4,6 +4,7 @@ import Hero from './components/hero.jsx'
 import ProductViewer from './components/Productviewer.jsx'
 import gsap from 'gsap'
 import { ScrollTrigger } from "gsap/all"
+import Showcase from './components/showcase.jsx'
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -13,6 +14,7 @@ const App = () => {
         <Navbar />
         <Hero />
         <ProductViewer />
+        <Showcase />
     </main>
   )
 }
