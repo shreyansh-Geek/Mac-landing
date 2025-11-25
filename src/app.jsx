6c05b0818fc99f5b0feab458ed0b0fbbe5a66b89
Shreyansh <shreyanshloop07@gmail.com>
2025-11-25 20:18:12 +0530

    implement full Features section with 3D Macbook model, scroll-synced animations, and dynamic texture updates

--- src/app.jsx
+++ src/app.jsx
@@ -6,6 +6,7 @@ import gsap from 'gsap'
 import { ScrollTrigger } from "gsap/all"
 import Showcase from './components/showcase.jsx'
 import Performance from './components/performance.jsx'
+import Features from './components/Features.jsx'
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -17,6 +18,7 @@ const App = () => {
         <ProductViewer />
         <Showcase />
         <Performance />
+        <Features />
     </main>
   )
 }
