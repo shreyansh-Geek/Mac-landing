763f69f9618dbcb5531a33f4ff71ea2d51e72e5b
Shreyansh <shreyanshloop07@gmail.com>
2025-11-25 16:24:22 +0530

    ProductViewer Section Implemented with dummy cube model

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar.jsx'
 import Hero from './components/hero.jsx'
+import ProductViewer from './components/Productviewer.jsx'
 
 const App = () => {
   return (
     <main>
         <Navbar />
         <Hero />
+        <ProductViewer />
     </main>
   )
 }
