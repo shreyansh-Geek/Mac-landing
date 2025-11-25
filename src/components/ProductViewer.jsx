82f8500418e70d3b30fa25c895bf93afab92db30
Shreyansh <shreyanshloop07@gmail.com>
2025-11-25 17:05:46 +0530

    Mac Model rendering ThreeJS

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -3,6 +3,8 @@ import useMacbookStore from '../store'
 import clsx from 'clsx'
 import { Canvas } from '@react-three/fiber'
 import { Box, OrbitControls } from '@react-three/drei'
+import MacbookModel14 from './models/Macbook-14'
+import StudioLights from './StudioLights'
 
 
 const ProductViewer = () => {
@@ -31,7 +33,8 @@ const ProductViewer = () => {
         </div>
         
         <Canvas id="canvas" camera={{position:[0,2,5], fov:50, near:0.1, far:100}}>
-            <Box position ={[-1, 1, 0]} scale={10*scale} material-color={color} />
+            <StudioLights />
+            <MacbookModel14 position ={[0, 0, 0]} scale={0.06}  />
             <OrbitControls enableZoom={false}/>
         </Canvas>
     </section>
